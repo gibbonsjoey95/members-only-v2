5633306264f582fdf6f94e90f7e7bb657733145e
Joey Gibbons <gibbonsjoey95@gmail.com>
2023-08-29 22:25:31 -0500

    kind of finished this project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+
+  has_many :posts, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'posts/new'
   get 'posts/create'
   devise_for :users
+
+  resources :posts
+  root to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
